b81309721fbacf0ad185727216e4819fde3fc32e
unknown <kauanviniciusmeronha87@gmail.com>
2024-09-20 00:27:47 -0300

    termino do arquivo tarefa

--- tarefas.php
+++ tarefas.php
@@ -1,3 +1,11 @@
+<?php session_start(); 
+    //É uma função de abertura para o uso da suber global $_SESSION
+    //Quando clicarmos no botão cadastrar apenas a tarefa nova é listada
+    //O php trabalha principalmente com web e neste caso o que acontece a cada nova requisição que faremos, seja pedindo uma página, seja enviando dados, é que o PHP interpreta tudo de novo e devolve apenas HTML para o navegador. Ele não lembra o que aconteceu na última requisição feita
+    //Mais para isso o PHP tem um recurso que auxiliará a solucionar isso
+    //Que é o caso da super global $_SESSION que vai manter os dados entre requisições
+    //Diferença entre a super global $_GET e $_SESSION, você usa mais o $_GET para ler informações e vou usar $_SESSION para escrever e ler informação.
+ ?>
 <html>
     <head>
         <title>Gerenciador de Tarefas</title>    
@@ -16,7 +24,7 @@
             </fieldset>
         </form>
         <?php 
-            // $_GET é utilizada para recuperar dados eviados para o servidor através do URL
+            // $_GET é uma super global utilizada para recuperar dados eviados para o servidor através do URL
             // exemplo: o if vai verificar se o índice nome existe através da função isset, caso exista ele vai exibir usando echo
             /* 
             if (isset($_GET['nome'])){
@@ -24,11 +32,23 @@
             }
             */
             
-            $lista_tarefa = array();
+            //Nesta linha de código o if ele verifica se foi escrito algo no input (nome) coloca dentro do array $listar_tarefa e imprime la embeixo no foreache
+            //$lista_tarefa = array();
             
-            if (isset($_GET['nome']))
-            {
-                $lista_tarefa[] = $_GET['nome'];
+            //if (isset($_GET['nome']))
+            //{
+            //    $lista_tarefa[] = $_GET['nome'];
+            //}
+
+            //aqui a mesma coisa só que usamos o SESSION para quarda requisições ateriores e imprimir todas as irformações inseridas mesmo atualizando a página
+            if(isset($_GET['nome'])){
+                $_SESSION['lista_tarefas'][] = $_GET['nome'];
+            }
+
+            $lista_tarefas = array();
+            
+            if (isset($_SESSION['lista_tarefas'])){
+                $lista_tarefas = $_SESSION['lista_tarefas'];
             }
         ?>
 
@@ -38,7 +58,7 @@
             </tr>
 
             <!-- ESsa linha de código tem outra forma de escrever usando com chaves, mas esta form usando dois ponto para abrir o bloco e a palavra endforech fica mais legível em templates HTML -->
-            <?php foreach ($lista_tarefa as $tarefa) : ?>
+            <?php foreach ($lista_tarefas as $tarefa) : ?>
                 <!-- foreach é um laço que serve para passar por todos índices de um array, atribuindo cada índice a uma variável que escolhemos -->
                 <tr>
                     <th><?php echo $tarefa; ?></th>
@@ -46,14 +66,14 @@
             <?php endforeach; ?>    
             <!-- existe outros blocos com a mesma sintaxe if e endif, while e endwhile e for e endfor -->
 
-            <!-- Exemplo do código acima usando chaves -->
-            <?php foreach ($lista_tarefa as $tarefa) { ?>
+            <!-- Exemplo do código acima usando chaves -->  
+            <?php //foreach ($lista_tarefa as $tarefa) { ?>
                 <!--
                 <tr>
                     <td><?php echo $tarefa; ?></td>
                 </tr>
                 -->
-            <?php } ?>
+            <?php //} ?>
         </table>
     </body>
 </html>
